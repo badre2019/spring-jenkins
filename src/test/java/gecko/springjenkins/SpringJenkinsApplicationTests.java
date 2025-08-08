4e94b2e848cc9e1473daece93214110eada86d98
Bader <b.fadel@agoraz.be>
2025-08-08 12:03:52 +0200

    comment a logger statement

--- src/test/java/gecko/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/gecko/springjenkins/SpringJenkinsApplicationTests.java
@@ -22,7 +22,7 @@ public class SpringJenkinsApplicationTests {
 	@Test
 	public void contextLoads() {
 		logger.info("test case executing...");
-		logger.info("test case executing second log statement...");
+//		logger.info("test case executing second log statement...");
 		assertEquals(true, true);
 
 	}
